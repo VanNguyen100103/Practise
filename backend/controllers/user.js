edf02e4e2a8239fc37c294b9b250962c0cb47f16
VanNguyen100103 <nganhvan1609@gmail.com>
2024-11-05 23:07:30 +0700

    third

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,53 +1,49 @@
-const User = require("../models/user");
 const asyncHandler = require("express-async-handler");
-const sendEmail = require("../utils/sendEmail");
+const User = require('../models/user');
 const crypto = require('crypto');
+const {generateAccessToken, generateRefreshToken} = require('../middlewares/generateToken');
+const sendEmail = require('../utils/sendEmail');
 const makeToken = require('uniquid');
-const { generateAccessToken, generateRefreshToken } = require("../middlewares/generateToken");
-
+const moment = require('moment-timezone');
 
 const register = asyncHandler(async (req, res) => {
-    const { firstname, lastname, mobile, address, email, password } = req.body;
-    if(!firstname || !lastname || !mobile || !address || !email || !password)  throw new Error("Missing inputs.");
-    const user = await User.findOne({ email });
+    const {firstname, lastname, mobile, address, email, password} = req.body
+    if(!firstname || !lastname || !mobile || !address || !email || !password) throw new Error("Missing inputs!");
+    const user = await User.findOne({email})
     if(user) throw new Error("User existed!");
     let avatar;
     if(req.file){
         avatar = {
             public_id: req.file.filename,
-            url: req.file.path
+            url: req.file.path,
         }
     }
     const token = makeToken()
-    res.cookie("dataRegister",{
-        ...req.body, 
-        avatar,
-        token
-    },{
-        maxAge: 24*60*60*1000,
+    res.cookie("dataRegister", {...req.body, avatar, token}, {
         httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
         sameSite: "lax"
     })
-    const html = `Please confirm your registration by clicking the link below:<br/><a href="${process.env.CLIENT_URL}/final-register/${token}">Confirm Registration</a>`;
-    await sendEmail({ email, html, subject: "Complete Your Registration" });
+    const html = `Xác nhận quá trình đăng ký trong vòng 24 giờ kể từ lúc nhận được mail.<br/>Xin quý khách click link phía dưới để đăng ký tài khoản.<br/><a href="${process.env.CLIENT_URL}/final-register/${token}">Xác nhận</a>`
+    await sendEmail({email, html, subject: "Confirm Registration"})
     return res.status(200).json({
         success: true,
-        mes: "Please check your email to confirm registration."
+        mes: "Please check your email to confirm registration!"
     })
 })
 
 const finalRegister = asyncHandler(async (req, res)=>{
-    const cookie = req.cookies
     const { token } = req.params
+    const cookie = req.cookies
     if(!cookie || !cookie.dataRegister || cookie.dataRegister.token !== token){
         return res.status(400).json({
-            success : false,
-            mes: "Registration failed due to invalid or missing token",
+            success: false,
+            mes: "Registration failed due to invalid or missing token."
         })
     }
-    const {  firstname, lastname, mobile, address, email, password, avatar } = cookie.dataRegister;
-    const user = await User.create({firstname, lastname, mobile, address, email, password, avatar })
+    const { firstname, lastname, mobile, address, email, password, avatar } = cookie.dataRegister
     res.clearCookie("dataRegister")
+    const user = await User.create({firstname, lastname, mobile, address, email, password, avatar })
     return res.status(200).json({
         success: user ? true : false,
         mes: user 
@@ -56,30 +52,46 @@ const finalRegister = asyncHandler(async (req, res)=>{
 
 const login = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
-    if (!email || !password) throw new Error("Missing inputs.");
+    if (!email || !password) throw new Error("Missing inputs!");
 
-    const user = await User.findOne({ email }).select("+password"); // Lấy mật khẩu đã mã hóa để so sánh
+    const user = await User.findOne({ email }).select("+password");
     if (!user) {
-        return res.status(400).json({ mes: "User didn't register!" });
+        return res.status(400).json({
+            success: false,
+            mes: "User hasn't registered."
+        });
     }
 
-    // So sánh mật khẩu
     const isMatch = await user.comparePassword(password);
     if (!isMatch) {
-        return res.status(400).json({ mes: "User entered wrong password!" });
+        return res.status(400).json({
+            success: false,
+            mes: "User entered wrong password."
+        });
     }
-
-    // Đăng nhập thành công
+    const lastLoginAt = new Date(user.lastLoginAt)
+    const oneYearAgo = new Date()
+    oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1)
+    // Kiểm tra nếu người dùng đã đăng nhập gần đây hơn một năm so với ngày hiện tại
+    if (lastLoginAt < oneYearAgo) {
+        user.isBlocked = true;
+        await user.save();
+        return res.status(403).json({ success: false, mes: "User is blocked!", isBlocked: user.isBlocked });
+    }
+ 
     const accessToken = generateAccessToken(user._id, user.role);
     const newRefreshToken = generateRefreshToken(user._id);
-
     res.cookie("refreshToken", newRefreshToken, {
         httpOnly: true,
-        maxAge: 12 * 30 * 24 * 60 * 60 * 1000,
-        sameSite: 'lax'
+        maxAge: 24 * 60 * 60 * 1000,
+        sameSite: "lax"
     });
 
-    await User.updateOne({ _id: user._id }, { lastLoginAt: Date.now() }); // Cập nhật thời gian đăng nhập cuối
+    await User.findByIdAndUpdate(
+        user._id,
+       {$set: { lastLoginAt: new Date(), isBlocked: false}},
+        { new: true }
+    );
 
     return res.status(200).json({
         success: true,
@@ -89,66 +101,54 @@ const login = asyncHandler(async (req, res) => {
             mobile: user.mobile,
             address: user.address,
             email: user.email,
+            avatar: user.avatar,
         },
         accessToken
     });
 });
 
 
+
+
 const forgotPassword = asyncHandler(async (req, res) => {
-    const { email } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) throw new Error("User didn't register");
-
-    // Tạo token đặt lại mật khẩu
-    const resetToken = user.createPasswordChangedToken();
-
-    // Lưu thay đổi vào cơ sở dữ liệu
-    await user.save(); 
-
-    // Gửi email với link reset password
-    const html = `Thời gian tồn tại của mã trong 24 giờ kể từ lúc nhận được.<br/>Xin quý khách click link bên dưới để đổi mật khẩu.<br/><a href="${process.env.CLIENT_URL}/forgot-password/${resetToken}">Confirm</a>`;
-    await sendEmail({ email, html, subject: "Forgot Password" });
-
-    return res.status(200).json({ success: true, mes: "Please check your email!" });
-});
-
-
-const resetPassword = asyncHandler(async (req, res) => {
-    const { token, password } = req.body;
-    if (!token || !password) throw new Error("Missing inputs.");
-
-    // Mã hóa token nhận được từ yêu cầu để so sánh
-    const passwordResetToken = crypto.createHash("sha256").update(token).digest("hex");
-
-    // Tìm người dùng với token khớp và token chưa hết hạn
-    const user = await User.findOne({
-        passwordResetToken,
-        passwordResetExpires: { $gt: Date.now() } // kiểm tra hạn của token
-    });
-
-    if (!user) {
+    const {email} = req.body
+    if(!email) throw new Error("Missing inputs!")
+    const {_id} = req.user
+    const user = await User.findById(_id)
+    if(!user) {
         return res.status(400).json({
-            success: false,
-            mes: "Invalid or expired token"
-        });
+            success : false,
+            mes: "User haven't registered"
+        })
     }
-
-    // Đặt lại mật khẩu và xóa các thuộc tính liên quan đến token
-    user.password = password;
-    user.passwordResetToken = undefined;
-    user.passwordResetExpires = undefined;
-    user.passwordChangedAt = Date.now();
-
-    await user.save();
-
+    const resetToken = await user.createPasswordChangedToken()
+    const html = `Xác nhận quá trình đổi mật khẩu trong vòng 24 giờ kể từ lúc nhận được mail.<br/>Xin quý khách click link phía dưới để đổi mật khẩu.<br/><a href="${process.env.CLIENT_URL}/reset-password">Xác nhận</a>.<br/>Mã để đổi mk:${resetToken}`
+    await sendEmail({email, html, subject: "Confirm Change Password"})
+    await user.save()
     return res.status(200).json({
         success: true,
-        mes: "Password changed successfully!"
-    });
-});
+        mes: "Please check your email to confirm change password."
+    })
+})
 
-
+const resetPassword = asyncHandler(async (req, res)=>{
+    const {password, token} = req.body
+    if(!token || !password) throw new Error("Missing inputs!")
+    const passwordResetToken = crypto.createHash("sha256").update(token).digest("hex")
+    const {_id} = req.user
+    const user = await User.findOne({_id: _id, passwordResetToken, passwordResetExpires: {$gt: Date.now()}})
+    if(!user) {
+        return res.status(400).json({success: false, mes: "User chưa đăng ký."})
+    }
+    user.password = password
+    user.passwordChangedAt = Date.now()
+    user.passwordResetExpires = undefined
+    user.passwordResetToken = undefined
+    await user.save()
+    return res.status(200).json({
+        success: true,
+        mes: "Change password successfully"
+    })
+})
 
 module.exports = {register, finalRegister, login, forgotPassword, resetPassword}
-  
--- backend/middlewares/verifyToken.js
+++ backend/middlewares/verifyToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const asyncHandler = require("express-async-handler");
 
 const verityToken = asyncHandler((req, res, next) => {
-    if(req?.headers?.authorization.startsWith("Bearer")) {
+    if(req?.headers?.authorization?.startsWith("Bearer")) {
         const token = req.headers.authorization.split(" ")[1]
         jwt.verify(token, process.env.JWT_SECRETKEY, (err, decode) => {
           if(err) return res.status(401).json({ success: false, mes: "Invalid accesstoken!"});
